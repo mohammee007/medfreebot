9d2089f531839cde24c57bbcaac585d9733434b6
mohammee007 <127308780+mohammee007@users.noreply.github.com>
2023-03-11 15:55:29 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# medfreebot
+<p align="center"> 
+<b>༺═════════[👸]══════════༻</b>
+</p>
+<p align="center">
+<img src="https://telegra.ph/file/c0c8a7440635f381fe098.jpg" width="300" height="300"/>
+</p>
+<p align="center">
+  <a href="#"><img src="http://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=1000&width=435&lines=BOBIZA+BOT+Whatsapp+Multidevice+Bot+;By+NOUREDDINE+OUAFY" alt="">
---
+++ settings.js
@@ -0,0 +1,161 @@
+/*
+===================================================================================================
+
+===================================================================================================
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+
+//👇 if you need seen massage use " true "
+global.READ_MASSAGE = 'true'
+//👇 enable desable Girls Voice Reply
+global.VOICE_REPLY =  'true'
+// You Bots Owner Number 
+global.owner = ['212617741471'] 
+//👇 Your Caption ( Image Video )
+global.cap = '© BOBIZA BOT ²⁰²³'
+//👇 Do you want show time on your bio use "on" want desable use "off"
+global.AUTO_BIO =  'on'
+//👇 Inbox massage block PM block
+global.INBOX_BLOCK =  'off'
+//👇 Auto react 
+global.AUTO_REACT =  'true'
+//👇 Bad word Auto delete ( you must add bad words )
+global.ANTI_BADWORD =  'true'
+//👇 Your Bot Name
+global.botnma =  'BOBIZABOT' 
+//👇 Your name
+global.ownernma =  'instagram.com/noureddine_ouafy' 
+//👇 Sticker package name
+global.packname =  'BOBIZA' 
+//👇 Sticker Author Name
+global.author =  '_NOUREDDINE_OUAFY_' 
+//👇 Kick And Auto Delete Group link Senders
+global.antilink =  'false'
+//👇 Inbox Block Massage 
+global.INBOX_BLOCK_MSG = 'BLOCK !!!'
+//👇 Owner React Imoji
+global.OWNER_REACT =  '♥'
+global.BAD_KICK_MSG = '*Bad word detect !*'
+//👇 URL.button 1 name
+global.BUTTON1 = 'instagram'
+//👇 URL button 2 name
+global.BUTTON2 = '𝚈𝚘𝚞𝚝𝚞𝚋𝚎'
+//👇 Button 1 url 
+global.BUTTON1_URL = 'https://instagram.com/noureddine_ouafy'
+//👇 Button 2 url
+global.BUTTON2_URL = 'https://youtube.com/c/@noureddinenouafy2'
+//👇 Your Add Massage 
+global.ADD_MSG = '*✅ Success add Group*' 
+//👇 Your Kick Massage 
+global.KICK_MSG = '*✅ Success Remove from Group*' 
+//👇 Song Download Massage 
+global.SONG_DOWN = '```⬇️ جاري تحميل الموسيقى تابعني هنا instagram.com/noureddine_ouafy...```' 
+//👇 Song Uplode massage 
+global.SONG_UP = '```⬆️ جاري الارسال   instagram.com/noureddine_ouafy...```'
+//👇 212 Number block
+global.NUMBER_212_BLOCK = 'false'
+//👇 Menu imoji 
+global.MENU_IMOJI = '😄|🪄'
+//👇 Your time zone ( use correct time zone )
+global.TIME_ZONE = process.env.TIME_ZONE || 'Asia/Colombo'
+//👇 Removebg Api key
+global.REMOVE_BG_API = 'aGSQ7rF4TnnUeytKEbX72fSN'
+//👇 Pron Video Download 
+global.SEX_VIDEO_DOWNLOAD = process.env.SEX_VIDEO_DOWNLOAD || 'false'
+//👇 Change Bot Language 
+global.LANG = 'EN'
+//Alive logo
+global.alivelogo = process.env.ALIVE_IMAGE || `https://telegra.ph/file/c0c8a7440635f381fe098.jpg`
+//👇 Alive message
+global.ALIVE_MESSAGE = process.env.ALIVE_MESSAGE || ``
+//👇 Welcome Mesaage
+global.WELCOME = process.env.WELCOME || ``
+//👇 welcome image 
+global.WELCOME_IMAGE = process.env.WELCOME_IMAGE || ``
+//👇 Goodbye message 
+global.GOODBYE = process.env.GOODBYE || ``
+//👇 Good bye image
+global.GOODBYE_IMAGE = process.env.GOODBYE_IMAGE || ``
+//👇 Send Welcome ( true or false )
+global.SEND_WELCOME = process.env.SEND_WELCOME || true
+//👇 Send block command message (true or false )
+global.BLOCK_CMD_MESSAGE_SEND = false
+//👇 Desable PM Block
+global.NO_BLOCK = ["212617741471"]
+
+
+
+
+
+
+
+//MORE
+global.zenzapi = '01ABEB1E11'
+global.tiktokthub = 'https://telegra.ph/file/c0c8a7440635f381fe098.jpg'
+//other
+global.pemilik = ['212617741471'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['212617741471'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = '_Noureddine Ouafy_' // Your name ඔබේ නම 
+global.sessionName = 'session'
+global.prefix = ['#','!','.'] 
+global.sp = '🔵'
+
+
+global.mess = {
+    success: '✅ تم!',
+    admin: '*Only admins can use this command !*',
+    botAdmin: '*Please admin the bot first !*',
+    owner: '*Sorry, this command is only for the owner !*',
+    group: '*This command can only be used in groups !*',
+    private: '*This command can only be used in Inbox !*',
+    bot: '*This command can only be used by bot number !*',
+    wait: '*♲ المرجو الانتظار قليلا سوف يتم تلبية طلبكم بعد قليل  تابعني هنا instagram.com/noureddine_ouafy ...*',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    BLOCK_CMD_MSG : '*⚠️ This Command is Block By Owner*',
+    BLOCK : '',
+    UNBLOCK : '',
+    KICK : '',
+    ADD : '',
+    LEAVE : '',
+    PROMOTE : '',
+    DEMOTE : ''
+}
+global.limitawal = {
+    premium: "Infinity", 
+    free: 90 
+}
+    global.rpg = {
+        darahawal: 100,
+        besiawal: 15,
+        goldawal: 10,
+        emeraldawal: 5,
+        umpanawal: 5,
+        potionawal: 1
+     }    
+
+
+
+
+//global api
+global.fbapi = 'dd79-1aeb-21a3'
+global.APIs = {
+	zenz: 'https://zenzapi.xyz', 
+}
+global.APIKeys = {
+	'https://zenzapi.xyz': '01ABEB1E11', //Kalau habis beli sendiri
+}
+
+global.thumb = fs.readFileSync('./Media/image/Nilu.jpg')
+global.alivelogo = `https://telegra.ph/file/c0c8a7440635f381fe098.jpg`
+global.imgalive = fs.readFileSync('./Media/image/Nilu.jpg')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
